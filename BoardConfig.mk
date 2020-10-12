7a80dc83bd6db0138a0d5fda3c11122494c99921
kubersharma001 <kubersharma001@gmail.com>
2020-10-12 12:17:15 +0200

    juice: Include QCOM sepolicy
    Change-Id: I96714846258d6c409ed9257da15ac2281bfd1d9b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,6 +97,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USES_MKE2FS := true
 
 # Sepolicy
+include device/qcom/sepolicy/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # UEFI
