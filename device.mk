cd051652be342b68ab435c66b5ba146e0d8ca51e
daniml3 <danimoral1001@gmail.com>
2021-03-29 15:58:12 +0200

    juice: Build fastbootd packages
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I14776da68eb2cf3e4a1a0c06da70f53a054da784

--- device.mk
+++ device.mk
@@ -9,6 +9,11 @@ BOARD_BUILD_PRODUCT_IMAGE := true
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock
+
 # File systems table
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.default:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
