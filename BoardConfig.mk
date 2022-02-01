580fc78903fca7cd7ccf9e0d00e4767cc36b3aec
frostg-012 <optimus7907@gmail.com>
2022-02-01 11:27:52 +0700

    juice: Set {system,product} properties
    Change-Id: I94cc78b0a1c1be0409d240501a55cfaf98d40480

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,6 +96,10 @@ TARGET_COPY_OUT_PRODUCT := product
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno610
 BOARD_USES_QCOM_HARDWARE := true
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/properties/product.prop
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.default
 BOARD_USES_RECOVERY_AS_BOOT := false
