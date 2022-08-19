7912e040cc1a26f7aafe4b03ccdadffa7fdb5a04
dragonroad99 <fadhilm298@gmail.com>
2022-08-19 12:07:40 +0700

    juice: change build type to unofficial

--- nad_juice.mk
+++ nad_juice.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/xiaomi/juice/device.mk)
 
 # Inherit common NusantaraProject configurations
 $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
-NAD_BUILD_TYPE := OFFICIAL
+NAD_BUILD_TYPE := UNOFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
 USE_PIXEL_CHARGING := true
 
