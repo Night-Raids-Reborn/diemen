e30c5eb6eddf8f718e4235303e943cd476882565
dragonroad99 <fadhilm298@gmail.com>
2022-08-19 18:30:58 +0000

    Juice: AncientOS BringUP

--- ancient_juice.mk
+++ ancient_juice.mk
@@ -7,17 +7,21 @@
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit common NusantaraProject configurations
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+# Inherit common Ancient configurations
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 NAD_BUILD_TYPE := UNOFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
 USE_PIXEL_CHARGING := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := nad_juice
+PRODUCT_NAME := ancient_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Ancient
+IS_PHONE := true
+export ANCIENT_NOGAPPS=true
--- device.mk
+++ device.mk
@@ -102,7 +102,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-nad
+    $(LOCAL_PATH)/overlay-anci
 
 PRODUCT_PACKAGES += \
     FrameworksResTarget
