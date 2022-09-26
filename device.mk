50436c42d8fd443aa1ac660028827ef39c517f88
darknius09 <darkniuslightness@gmail.com>
2022-09-26 03:48:57 +0000

    Revert "juice: Support Alipay fingerprint payment"
    This reverts commit 8c1556842ead923386a813f2665d77104e7cbe81.

--- device.mk
+++ device.mk
@@ -69,14 +69,6 @@ PRODUCT_PACKAGES += \
     init.juice.rc
 
 # Init
-# IFAA manager
-PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
-
-# IPACM
 PRODUCT_PACKAGES += \
     init.mi_thermald.rc
 
